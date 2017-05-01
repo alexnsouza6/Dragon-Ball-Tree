3f1e969e902ff9400362422f3965fea79f91f24d
alexnsouza6 <alexnsouza6@hotmail.com>
2017-05-01 00:29:25 -0300

    Inserção do file com PlayAPC

--- Teste.c
+++ Teste.c
@@ -9,16 +9,16 @@ int main(){
     AbreJanela(400, 500, "Montagem");
     PintarFundo(0, 0, 0);
 
-    img = AbreImagem("./personagem_0006s_0001_humano.png");
+    img = AbreImagem("/personagem/raca/personagem_mutante.png");
 
     p.x = -75;
     p.y = -90;
-    CriaRetangulo(150,180, p);
+    CriaRetangulo(150,180,p);
     Pintar(255, 255, 255);
     AssociaImagem(img);
 
 
-    img = AbreImagem("./personagem_0005s_0001_olho-redondo.png");
+    img = AbreImagem("personagem/olho_formato/personagem_0005s_0001_olho-redondo.png");
     p.x = -12;
     p.y = 70;
     CriaRetangulo(20, 16, p);
@@ -26,7 +26,7 @@ int main(){
     AssociaImagem(img);
 
 
-    img = AbreImagem("./personagem_0004s_0000_olho-azul.png");
+    img = AbreImagem("personagem/olho_cor/personagem_0004s_0000_olho-azul.png");
     p.x = -8;
     p.y = 73;
     CriaRetangulo(13, 6, p);
@@ -34,7 +34,7 @@ int main(){
     AssociaImagem(img);
 
 
-    img = AbreImagem("./personagem_0000s_0000_camisa.png");
+    img = AbreImagem("personagem/Camisas/personagem_0000s_0000_camisa.png");
 
     p.x = -50;
     p.y = -10;
